f8906d185ada8a99a7bd6af1ad06a3adf35ff16b
szTheory <szTheory@users.noreply.github.com>
2019-12-08 01:06:10 -0500

    enable auto-updater

--- src/main/index.js
+++ src/main/index.js
@@ -9,7 +9,7 @@ if (is.development && module.hot) {
 
 const path = require('path')
 const { app, BrowserWindow, Menu, ipcMain } = require('electron')
-/// const {autoUpdater} = require('electron-updater')
+const { autoUpdater } = require('electron-updater')
 const unhandled = require('electron-unhandled')
 const debug = require('electron-debug')
 const contextMenu = require('electron-context-menu')
@@ -25,14 +25,14 @@ app.setAppUserModelId(packageJson.build.appId)
 
 // Uncomment this before publishing your first version.
 // It's commented out as it throws an error if there are no published versions.
-// if (!is.development) {
-//   const FOUR_HOURS = 1000 * 60 * 60 * 4
-//     setInterval(() => {
-//       autoUpdater.checkForUpdates()
-//   }, FOUR_HOURS)
-//
-//   autoUpdater.checkForUpdates()
-// }
+if (!is.development) {
+  const FOUR_HOURS = 1000 * 60 * 60 * 4
+  setInterval(() => {
+    autoUpdater.checkForUpdates()
+  }, FOUR_HOURS)
+
+  autoUpdater.checkForUpdates()
+}
 
 // Prevent window from being garbage collected
 let mainWindow
