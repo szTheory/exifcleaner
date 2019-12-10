805c7a9c93e45cebb07ac330ee3bc867928f08c3
szTheory <szTheory@users.noreply.github.com>
2019-12-10 10:48:20 -0500

    downgrade to Electron 6 to fix auto-update

--- src/main/dark_mode.js
+++ src/main/dark_mode.js
@@ -3,7 +3,11 @@ import { systemPreferences, nativeTheme } from "electron";
 export const DARK_MODE_MESSAGE_NAME = "dark-mode";
 
 export const isDarkMode = function() {
-	return nativeTheme.shouldUseDarkColors;
+	// TODO: switch to this after upgrade to Electron 7
+	// return nativeTheme.shouldUseDarkColors;
+	// systemPreferences for Electron 6 compat
+	// source: electronjs.org/docs/tutorial/mojave-dark-mode-guide
+	return systemPreferences.isDarkMode();
 };
 
 export const listenForDarkMode = function({ win }) {
