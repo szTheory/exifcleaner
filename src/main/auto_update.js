3cc0edd7a605faa7f25e4540c09e26890c761ea4
szTheory <szTheory@users.noreply.github.com>
2019-12-09 23:54:04 -0500

    autoupdate logging

--- src/main/auto_update.js
+++ src/main/auto_update.js
@@ -3,16 +3,28 @@ import { autoUpdater } from "electron-updater";
 
 const FOUR_HOURS = 1000 * 60 * 60 * 4;
 
+function setupLogging() {
+	// LOGGING
+	autoUpdater.logger = require("electron-log");
+	autoUpdater.logger.transports.file.level = "info";
+}
+
+function checkPeriodically() {
+	setInterval(() => {
+		autoUpdater.checkForUpdates();
+	}, FOUR_HOURS);
+}
+
+function checkNow() {
+	autoUpdater.checkForUpdates();
+}
+
 export const setupAutoUpdate = function() {
 	if (is.development) {
 		return;
 	}
 
-	// periodically check
-	setInterval(() => {
-		autoUpdater.checkForUpdates();
-	}, FOUR_HOURS);
-
-	// check now
-	autoUpdater.checkForUpdates();
+	setupLogging();
+	checkPeriodically();
+	checkNow();
 };
